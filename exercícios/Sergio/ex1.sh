9eaaf5da812e801e84c0913ff4bd385e2ce92c60
Sergio Lemos Hermsdorff <sergiolemoshff@gmail.com>
2020-08-12 10:31:23 -0300

    Exercício 1

--- exercícios/Sergio/ex1.sh
+++ exercícios/Sergio/ex1.sh
@@ -1,5 +1,5 @@
 main(){
-        #Para cada i em sequencia de 1 a 100, a cada 2 números, escreva o número (números pares de 1 à 100)
+        #Para cada i em sequencia de 1 a 100, a cada 2 números, escreva o número (números pares de 1 à 100).
         for i in `seq 1 2 100` ; do
                 echo "$i"
         done
