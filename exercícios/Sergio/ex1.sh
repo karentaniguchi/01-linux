b3b0a3ea87fb0f862ce349ee14f677764a9b0845
Sergio Lemos Hermsdorff <sergiolemoshff@gmail.com>
2020-08-12 10:27:41 -0300

    Create ex1.sh

---
+++ exercícios/Sergio/ex1.sh
@@ -0,0 +1,8 @@
+main(){
+        #Para cada i em sequencia de 1 a 100, a cada 2 números, escreva o número (números pares de 1 à 100)
+        for i in `seq 1 2 100` ; do
+                echo "$i"
+        done
+}
+
+main
