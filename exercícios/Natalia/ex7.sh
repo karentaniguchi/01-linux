300fccddeaaa73c0a441271d029afc9b5e4fbac6
nataliaasaa <nataliaasaa@gmail.com>
2020-08-13 11:32:41 -0300

    Update ex7.sh

--- exercícios/Natalia/ex7.sh
+++ exercícios/Natalia/ex7.sh
@@ -1,6 +1,8 @@
+#parametro 1: numero
+#parametro 2: nome de um arquivo
 num="$1"
 nome_do_arquivo="$2"
-for i in `seq 1 $num`; do
-numerogerado=$RANDOM
-echo $numerogerado >> $nome_do_arquivo
-done
+for i in `seq 1 $num`; do  #loop de uma sequencia de 1 até o numero do parametro
+numerogerado=$RANDOM   #define numero gerado como aleatório
+echo $numerogerado >> $nome_do_arquivo  #imprime o numero gerado e salva no arquivo colocado como parametro
+done #termina
